c98050f83a1916970ee4dcf9b1d7366e28a19bc7
NaokiMatsuzawa <naokim1004@gmail.com>
2022-12-07 05:18:57 +0900

    とりあえずテストを通るようにした

--- sample002/src/polish_notation/mod.rs
+++ sample002/src/polish_notation/mod.rs
@@ -1,27 +1,72 @@
+enum PolishNotationOperator{
+    Add,
+    Sub,
+}
+
 enum PolishNotationNode{
     Numeric(i32),
-    Operator(char),
-    Empty,
+    Operator(PolishNotationOperator),
 }
 
 struct PolishNotation{
-
+    node : PolishNotationNode,
+    left : Box<Option<PolishNotation>>,
+    right: Box<Option<PolishNotation>>,
 }
 
 impl PolishNotation{
     fn new(notation_str: String) -> Self{
-        PolishNotation {  }
+        Self::new_sub(&mut notation_str.split_whitespace().rev().collect()).unwrap()
+    }
+
+    fn new_sub(notation_str_vec : &mut Vec<&str>) -> Option<Self>{
+        let str_option = notation_str_vec.pop();
+        if str_option == None{
+            return None;
+        }
+        let str = str_option.unwrap();
+        if let Ok(value) = i32::from_str_radix(str, 10){
+            let node = PolishNotationNode::Numeric(value);
+            return Some(PolishNotation{node, left : Box::new(None), right : Box::new(None)});
+        }
+
+        let operator : PolishNotationOperator;
+        match str{
+            "+" => operator = PolishNotationOperator::Add,
+            "-" => operator = PolishNotationOperator::Sub,
+            _ => panic!("invalid operator")
+        }
+        let node = PolishNotationNode::Operator(operator);
+        Some(PolishNotation{node, left : Box::new(Self::new_sub(notation_str_vec)), right : Box::new(Self::new_sub(notation_str_vec))})
     }
 
     fn calc(&self)->i32{
-        0
+        match &self.node{
+            PolishNotationNode::Numeric(number) => *number,
+            PolishNotationNode::Operator(operator) =>{
+                match (self.left.as_ref(), self.right.as_ref()) {
+                    (Some(l), Some(r)) =>{
+                        match operator{
+                            PolishNotationOperator::Add => l.calc() + r.calc(),
+                            PolishNotationOperator::Sub => l.calc() - r.calc()
+                        }
+                    }
+                    _ => panic!("invalid")
+                }
+            }
+        }
     }
+}
+
+#[test]
+fn test_numeric_only(){
+    assert_eq!(PolishNotation::new("1".to_string()).calc(), 1);
 }
 
 #[test]
 fn test_simple_add(){
     assert_eq!(PolishNotation::new("+ 1 1".to_string()).calc(), 2);// 1 + 1 = 2
-    assert_eq!(PolishNotation::new("+ 10 990".to_string()).calc(), 2); //10 + 990 = 1000
+    assert_eq!(PolishNotation::new("+ 10 990".to_string()).calc(), 1000); //10 + 990 = 1000
 }
 
 #[test]
