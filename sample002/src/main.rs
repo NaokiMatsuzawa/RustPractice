c9c4bc00291bc1c7f7091d39ddf7ba81f1504fef
NaokiMatsuzawa <naokim1004@gmail.com>
2022-12-09 01:29:55 +0900

    ポーランド式の構造体を改修。外部からは文字列から計算させるようにした

--- sample002/src/main.rs
+++ sample002/src/main.rs
@@ -1,6 +1,7 @@
 mod polish_notation;
 
 fn main(){
-    let notation = polish_notation::PolishNotation::new("+ + 1 2 3".to_string());
-    println!("{}", notation.calc());
+    //let notation = polish_notation::PolishNotation::new("+ + 1 2 3".to_string());
+    let ans = polish_notation::PolishNotation::calc_from_str("+ 1 1909");
+    println!("{}", ans);
 }
--- sample002/src/polish_notation/mod.rs
+++ sample002/src/polish_notation/mod.rs
@@ -5,29 +5,31 @@ enum PolishNotationOperator{
 
 enum PolishNotationNode{
     Numeric(i32),
-    Operator(PolishNotationOperator),
+    Operator(PolishNotationOperator, Box<PolishNotation>, Box<PolishNotation>),
 }
 
 pub struct PolishNotation{
     node : PolishNotationNode,
-    left : Box<Option<PolishNotation>>,
-    right: Box<Option<PolishNotation>>,
 }
 
 impl PolishNotation{
-    pub fn new(notation_str: String) -> Self{
-        Self::new_sub(&mut notation_str.split_whitespace().rev().collect()).unwrap()
+    pub fn calc_from_str(notation_str: &str) -> i32{
+        Self::new(notation_str.to_string()).calc()
     }
 
-    fn new_sub(notation_str_vec : &mut Vec<&str>) -> Option<Self>{
+    fn new(notation_str: String) -> Self{
+        Self::new_sub(&mut notation_str.split_whitespace().rev().collect())
+    }
+
+    fn new_sub(notation_str_vec : &mut Vec<&str>) -> Self{
         let str_option = notation_str_vec.pop();
         if str_option == None{
-            return None;
+            panic!("invalid input string!");
         }
         let str = str_option.unwrap();
         if let Ok(value) = i32::from_str_radix(str, 10){
             let node = PolishNotationNode::Numeric(value);
-            return Some(PolishNotation{node, left : Box::new(None), right : Box::new(None)});
+            return PolishNotation{node};
         }
 
         let operator : PolishNotationOperator;
@@ -36,22 +38,17 @@ impl PolishNotation{
             "-" => operator = PolishNotationOperator::Sub,
             _ => panic!("invalid operator")
         }
-        let node = PolishNotationNode::Operator(operator);
-        Some(PolishNotation{node, left : Box::new(Self::new_sub(notation_str_vec)), right : Box::new(Self::new_sub(notation_str_vec))})
+        let node = PolishNotationNode::Operator(operator, Box::new(Self::new_sub(notation_str_vec)), Box::new(Self::new_sub(notation_str_vec)));
+        PolishNotation{node}
     }
 
-    pub fn calc(&self)->i32{
+    fn calc(&self)->i32{
         match &self.node{
             PolishNotationNode::Numeric(number) => *number,
-            PolishNotationNode::Operator(operator) =>{
-                match (self.left.as_ref(), self.right.as_ref()) {
-                    (Some(l), Some(r)) =>{
-                        match operator{
-                            PolishNotationOperator::Add => l.calc() + r.calc(),
-                            PolishNotationOperator::Sub => l.calc() - r.calc()
-                        }
-                    }
-                    _ => panic!("invalid")
+            PolishNotationNode::Operator(operator, left, right) =>{
+                match operator{
+                    PolishNotationOperator::Add => left.calc() + right.calc(),
+                    PolishNotationOperator::Sub => left.calc() - right.calc(),
                 }
             }
         }
@@ -60,17 +57,17 @@ impl PolishNotation{
 
 #[test]
 fn test_numeric_only(){
-    assert_eq!(PolishNotation::new("1".to_string()).calc(), 1);
+    assert_eq!(PolishNotation::calc_from_str("1"), 1);
 }
 
 #[test]
 fn test_simple_add(){
-    assert_eq!(PolishNotation::new("+ 1 1".to_string()).calc(), 2);// 1 + 1 = 2
-    assert_eq!(PolishNotation::new("+ 10 990".to_string()).calc(), 1000); //10 + 990 = 1000
+    assert_eq!(PolishNotation::calc_from_str("+ 1 1"), 2);// 1 + 1 = 2
+    assert_eq!(PolishNotation::calc_from_str("+ 10 990"), 1000); //10 + 990 = 1000
 }
 
 #[test]
 fn test_simple_sub(){
-    assert_eq!(PolishNotation::new("- 1 1".to_string()).calc(), 0); //1 - 1 = 0
-    assert_eq!(PolishNotation::new("- 10 100".to_string()).calc(), -90); //10 - 100 = -90
+    assert_eq!(PolishNotation::calc_from_str("- 1 1"), 0); //1 - 1 = 0
+    assert_eq!(PolishNotation::calc_from_str("- 10 100"), -90); //10 - 100 = -90
 }
