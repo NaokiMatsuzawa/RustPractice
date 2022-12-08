03a8b745a287608d8d5ed08fe967da23ac376067
NaokiMatsuzawa <naokim1004@gmail.com>
2022-12-09 02:52:55 +0900

    フォルダ構成を変更

--- sample002/src/main.rs
+++ sample002/src/main.rs
@@ -1,7 +1,7 @@
-mod polish_notation;
+mod calculator;
 
 fn main(){
     //let notation = polish_notation::PolishNotation::new("+ + 1 2 3".to_string());
-    let ans = polish_notation::PolishNotation::calc_from_str("+ 1 1909");
+    let ans = calculator::polish_notation::PolishNotation::calc_from_str("+ 1 1909");
     println!("{}", ans);
 }
