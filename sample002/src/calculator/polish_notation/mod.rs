f803adf0ed8d200199ae4802efc20d2d0765e7ea
NaokiMatsuzawa <naokim1004@gmail.com>
2022-12-14 11:56:01 +0900

    削除漏れ

--- sample002/src/calculator/polish_notation/mod.rs
+++
@@ -1,6 +0,0 @@
-mod polish_notation;
-mod operator; // 本当はpolish_notationしか見えなくしたい
-
-pub fn calc_from_formula(formula: &str) -> Result<i32, String>{
-    polish_notation::calc_from_formula(formula)
-}
--- sample002/src/main.rs
+++
@@ -1,7 +0,0 @@
-mod calculator;
-
-fn main(){
-    //let notation = polish_notation::PolishNotation::new("+ + 1 2 3".to_string());
-    let ans = calculator::polish_notation::calc_from_formula("+ 1 1909");
-    println!("{}", ans.unwrap());
-}
