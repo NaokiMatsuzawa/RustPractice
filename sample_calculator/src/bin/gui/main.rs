21ce9584e809fbf7cb9db75e4528542c38911663
NaokiMatsuzawa <naokim1004@gmail.com>
2022-12-21 13:36:37 +0900

    列挙型をうまく用いて、数値、演算子、空白の挿入処理の重複を削除

--- sample_calculator/src/bin/gui/main.rs
+++ sample_calculator/src/bin/gui/main.rs
@@ -12,20 +12,8 @@ struct Calculator{
 
 #[derive(Debug, Clone)]
 enum Message{
-    Number_0_Pressed,
-    Number_1_Pressed,
-    Number_2_Pressed,
-    Number_3_Pressed,
-    Number_4_Pressed,
-    Number_5_Pressed,
-    Number_6_Pressed,
-    Number_7_Pressed,
-    Number_8_Pressed,
-    Number_9_Pressed,
-    Space_Pressed,
-    Plus_Pressed,
-    Minus_Pressed,
-    Calc_Pressed,
+    InsertButtonPressed(char),
+    CalcButtonPressed,
 }
 
 impl Sandbox for Calculator{
@@ -41,20 +29,8 @@ impl Sandbox for Calculator{
 
     fn update(&mut self, message: Self::Message) {
         match message{
-            Message::Number_0_Pressed => self.formula.push('0'),
-            Message::Number_1_Pressed => self.formula.push('1'),
-            Message::Number_2_Pressed => self.formula.push('2'),
-            Message::Number_3_Pressed => self.formula.push('3'),
-            Message::Number_4_Pressed => self.formula.push('4'),
-            Message::Number_5_Pressed => self.formula.push('5'),
-            Message::Number_6_Pressed => self.formula.push('6'),
-            Message::Number_7_Pressed => self.formula.push('7'),
-            Message::Number_8_Pressed => self.formula.push('8'),
-            Message::Number_9_Pressed => self.formula.push('9'),
-            Message::Space_Pressed => self.formula.push(' '),
-            Message::Plus_Pressed => self.formula.push('+'),
-            Message::Minus_Pressed => self.formula.push('-'),
-            Message::Calc_Pressed => {
+            Message::InsertButtonPressed(c) => self.formula.push(c),
+            Message::CalcButtonPressed => {
                 let result = sample_calculator::calculator::polish_notation::calc_from_formula(&self.formula);
                 self.formula = match result{
                     Ok(value) => value.to_string(),
@@ -68,14 +44,14 @@ impl Sandbox for Calculator{
         use Message::*;
         column![
             text(&self.formula).size(50),
-            row![button("7").on_press(Number_7_Pressed), button("8").on_press(Number_8_Pressed), button("9").on_press(Number_9_Pressed)],
-            row![button("4").on_press(Number_4_Pressed), button("5").on_press(Number_5_Pressed), button("6").on_press(Number_6_Pressed)],
-            row![button("1").on_press(Number_1_Pressed), button("2").on_press(Number_2_Pressed), button("3").on_press(Number_3_Pressed)],
-            row![button("0").on_press(Number_1_Pressed), button("SPACE").on_press(Space_Pressed)],
-            row![button("+").on_press(Plus_Pressed), button("-").on_press(Minus_Pressed), button("=").on_press(Calc_Pressed)],
+            row![button("7").on_press(InsertButtonPressed('7')), button("8").on_press(InsertButtonPressed('8')), button("9").on_press(InsertButtonPressed('9'))].spacing(5).padding(5),
+            row![button("4").on_press(InsertButtonPressed('4')), button("5").on_press(InsertButtonPressed('5')), button("6").on_press(InsertButtonPressed('6'))].spacing(5).padding(5),
+            row![button("1").on_press(InsertButtonPressed('1')), button("2").on_press(InsertButtonPressed('2')), button("3").on_press(InsertButtonPressed('3'))].spacing(5).padding(5),
+            row![button("0").on_press(InsertButtonPressed('0')), button("SPACE").on_press(InsertButtonPressed(' '))].spacing(5).padding(5),
+            row![button("+").on_press(InsertButtonPressed('+')), button("-").on_press(InsertButtonPressed('-')), button("=").on_press(CalcButtonPressed)].spacing(5).padding(5),
         ]
         .padding(20)
-        .align_items(Alignment::Center)
+        .align_items(Alignment::Start)
         .into()
     }
 
